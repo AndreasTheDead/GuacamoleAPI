c2e3e5455ec42dcf8370f74dd0a7bb2ceac68bb8
AndreasTheDead <andreas@zeller.sh>
2024-06-18 22:25:28 +0200

    added definition of requred params and added error handlening for datasource selction

--- Public/ps1/Connect-GuacamoleAPI.ps1
+++ Public/ps1/Connect-GuacamoleAPI.ps1
@@ -1,9 +1,9 @@
 function Connect-GuacamoleAPI {
     param (
-        [pscredential]$credentials,
+        [pscredential][Parameter(Position=0,mandatory=$true)]$credentials,
         #[string][ValidatePattern("(^(http(s)?:\/\/.)[-a-zA-Z0-9:./]{2,256}$)")]$BaseURL,
-        [string][ValidateScript({$_ -match "(^(http(s)?:\/\/.)[-a-zA-Z0-9:./]{2,256}$)" -and $_ -notlike "*/" -and $_ -notlike "*/api*"})]$BaseURL,
-        [string][ValidateSet("mysql","sqlserver","mysql")]$DataSource
+        [string][Parameter(Position=1,mandatory=$true)][ValidateScript({$_ -match "(^(http(s)?:\/\/.)[-a-zA-Z0-9:./]{2,256}$)" -and $_ -notlike "*/" -and $_ -notlike "*/api*"})]$BaseURL,
+        [string][Parameter(Position=2,mandatory=$false)][ValidateSet("mysql","sqlserver","postgresql","mysql-shared","sqlserver-shared","postgresql-shared")]$DataSource
     )
     $URI = "$BaseURL/api/tokens"
     Write-Debug "Request URL = $URI"
@@ -22,7 +22,18 @@ function Connect-GuacamoleAPI {
         $script:GuacamoleConnection = [GuacamoleAPIConnection]::new()
         $script:GuacamoleConnection.BaseURL = $BaseURL
         $script:GuacamoleConnection.Token = (ConvertTo-SecureString $res.authToken -AsPlainText -Force)
-        $script:GuacamoleConnection.DataSource = $res.dataSource
+        
+        if($DataSource){
+            if($DataSource -in $res.availableDataSources){
+                $script:GuacamoleConnection.DataSource = $DataSource
+            }else{
+                Write-Error "Selected DataSource is not avalibale, falling back to Token respone source: $($res.dataSource)"
+                $script:GuacamoleConnection.DataSource = $res.dataSource
+            }
+        }else{
+            $script:GuacamoleConnection.DataSource = $res.dataSource
+        }
+        
         Write-Debug "GuacamoleConnectionObject:"
         Write-Debug "BaseURL: $($script:GuacamoleConnection.BaseURL)"
         Write-Debug "DataSource: $($script:GuacamoleConnection.DataSource)"
